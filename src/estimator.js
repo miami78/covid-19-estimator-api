0489ebbc51fbcf1afaeb06465ebd5636f941a8e5
Miami78 <miamilarry78@gmail.com>
2020-04-10 22:50:43 +0300

    initial commit

---
+++ src/estimator.js
@@ -0,0 +1,30 @@
+import {
+  impactCalculator
+} from './helper';
+
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: impactCalculator({ ...data }, 10),
+  severeImpact: impactCalculator({ ...data }, 50)
+});
+
+export const formatAPIResponse = (estimateValues) => covid19ImpactEstimator(estimateValues);
+
+// export const formatAPIResponse = (estimateValues) => {
+//   const { impact, severeImpact, data } = covid19ImpactEstimator(estimateValues);
+//   const estimatedValues = {
+//     data,
+//     estimate: {
+//       impact, severeImpact
+//     }
+//   };
+
+//   return estimatedValues;
+// };
+
+export const jsonResponse = (request, response) => {
+  const result = request.body;
+  response.status(200).send(formatAPIResponse(result));
+};
+
+export default covid19ImpactEstimator;
